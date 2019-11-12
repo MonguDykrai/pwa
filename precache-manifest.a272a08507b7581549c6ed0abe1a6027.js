35627cc6a84da1cd10f4ee4d73a906ae607e3127
jicf <jicf@zylliondata.com>
2019-11-12 19:52:28 +0800

    init

---
+++ precache-manifest.a272a08507b7581549c6ed0abe1a6027.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668253de1f51d2de4305",
+    "url": "/css/app.a510f641.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e3556baf49ffb37aaf5ba0c3a4a106c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "668253de1f51d2de4305",
+    "url": "/js/app.5a5b91ab.js"
+  },
+  {
+    "revision": "31e08f1e1bbf00633c44",
+    "url": "/js/chunk-vendors.6ad48f61.js"
+  },
+  {
+    "revision": "7d065064b1508ff4047c7d8b4fd98709",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
